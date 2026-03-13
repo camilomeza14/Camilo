126080ae21f08cf4efafd21c229f2c10dc9ed4b0
camilomeza14 <cmeza555@hotmail.com>
2026-03-12 23:07:51 -0500

    Update ejercicio#15.py

--- ejercicio#15.py
+++ ejercicio#15.py
@@ -2,25 +2,40 @@ tarifa_carro = 4000
 tarifa_moto = 2000 
 contador_carro = 0
 contador_moto = 0
-vehiculo_mas_pago =" "
+total_recaudado = 0
 pago_mayor = 0
-for i in range(1,2,1):
+vehiculo_mas_pago = ""
+
+
+for i in range(8):
+    print("\n--- Registro del vehículo ---")
+    
     placa = input("Ingrese la placa: ")
     tipo_de_vehiculo = input("Carro o Moto: ")
-    horas_parqueado = int(input("Cuantas horas llevas parqueado?: "))
+    horas_parqueado = int(input("¿Cuántas horas llevas parqueado?: "))
     
     if tipo_de_vehiculo == "Carro": 
-        contador_carro += 1 
+        contador_carro = contador_carro + 1 
         tarifa = tarifa_carro
     elif tipo_de_vehiculo == "Moto":
-        contador_moto += 1 
+        contador_moto = contador_moto + 1 
         tarifa = tarifa_moto
 
-
-    pagado = tarifa * horas_parqueado 
-
-    if pagado > pago_mayor:
-        pago_mayor= pagado
-        vehiculo_mas_pago = f"El producto que mas pagó es: {tipo_de_vehiculo}  placa {placa}"
     
-    print("vehiculo_mas_pago") 
+    pagado = tarifa * horas_parqueado 
+    print("Este vehículo debe pagar: $", pagado)
+
+    
+    total_recaudado = total_recaudado + pagado
+
+    
+    if pagado > pago_mayor:
+        pago_mayor = pagado
+        vehiculo_mas_pago = "El vehículo que más pagó es el de placa " + placa + " con un total de $" + str(pagado)
+    
+
+print("\n--- REPORTE FINAL ---")
+print("Total recaudado en el día: $", total_recaudado)
+print("Cantidad de carros:", contador_carro)
+print("Cantidad de motos:", contador_moto)
+print(vehiculo_mas_pago)
