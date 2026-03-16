406f4a56ee90b2c5544bd84fd6105ab5db7691ca
camilomeza14 <cmeza555@hotmail.com>
2026-03-16 14:37:51 -0500

    Delete square.py

--- square.py
+++
@@ -1,8 +0,0 @@
-#calcular el perimetro de un cuadrado 
-side = input("Please enter the length of one side of the square: ")
-
-side = int(side) 
-
-perimeter = 4 * side
-
-print("The perimeter of the square is:" , perimeter)
